ffe21b8d3540c2be707ab27cca635c0e8b94b520
davide orienti <dav.orienti@stud.uniroma3.it>
2022-07-08 11:11:48 +0200

    repository validator

---
+++ Immobiliare/src/main/java/it/uniroma3/siw/validator/ImmobileValidator.java
@@ -0,0 +1,35 @@
+package it.uniroma3.siw.validator;
+
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import it.uniroma3.siw.model.Immobile;
+
+public class ImmobileValidator implements Validator {
+
+	 @Autowired
+	 private ImmobileService immobileService;
+	  private static final Logger logger = LoggerFactory.getLogger(ImmobileValidator.class);
+
+	@Override
+	public boolean supports(Class<?> clazz) {
+		return Immobile.class.equals(clazz);
+	}
+
+	@Override
+	public void validate(Object obj, Errors errors) {
+		if (!errors.hasErrors()) {
+			 logger.debug("confermato: valori non nulli");
+		 if(immobileService.alreadyExist((Immobile)obj)) {
+			 logger.debug("e' un duplicato");
+	            errors.reject("Immobile.duplicato");
+	        }
+		}
+	}
+
+}
---
+++ Immobiliare/src/main/java/it/uniroma3/siw/validator/TicketValidator.java
@@ -0,0 +1,33 @@
+package it.uniroma3.siw.validator;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import it.uniroma3.siw.model.Immobile;
+
+public class TicketValidator implements Validator {
+
+	 @Autowired
+	 private TicketService ticketService;
+	  private static final Logger logger = LoggerFactory.getLogger(ImmobileValidator.class);
+
+	@Override
+	public boolean supports(Class<?> clazz) {
+		return Immobile.class.equals(clazz);
+	}
+
+	@Override
+	public void validate(Object obj, Errors errors) {
+		if (!errors.hasErrors()) {
+			 logger.debug("confermato: valori non nulli");
+		 if(ticketService.alreadyExist((Immobile)obj)) {
+			 logger.debug("e' un duplicato");
+	            errors.reject("Immobile.duplicato");
+	        }
+		}
+	}
+
+}
